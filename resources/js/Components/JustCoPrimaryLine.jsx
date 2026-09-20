a67779ced06ae17487261dbb28c0bbc5e54b93d6
Hilal Muzaki <hilalmuzakihilal@gmail.com>
2026-09-20 21:54:39 +0700

    fix : any problem

---
+++ resources/js/Components/JustCoPrimaryLine.jsx
@@ -0,0 +1,7 @@
+export default function PrimaryLine() {
+    return (
+        <div className="">
+            <hr className="bg-coPrimary/30 h-[1px] border-none" />
+        </div>
+    );
+}
--- resources/js/Navigations/TopNav.jsx
+++ resources/js/Navigations/TopNav.jsx
@@ -14,14 +14,9 @@ export default function TopNav() {
         <nav className="top-nav-parent-1">
             <Link href={route("home")} className="platform-name-size-1">
                 <img
-                    src="/images/funfo-logo-primary.png"
-                    alt=""
-                    className="dark:hidden block h-7"
-                />
-                <img
                     src="/images/funfo-logo-second.png"
                     alt=""
-                    className="hidden dark:block h-7"
+                    className="h-7"
                 />
             </Link>
         </nav>
